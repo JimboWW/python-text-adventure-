d444ba442436ad1b96cbd5eb42d7f30fb3c7a9e9
Jim Wilbur <45021912+JimboWW@users.noreply.github.com>
2021-07-31 16:28:01 -0700

    Update player.py

--- player.py
+++ player.py
@@ -1,25 +1,47 @@
-import items
+mport items
+import world
 
 class Player:
     def __init__(self):
         self.inventory = [items.Rock(),
                           items.Dagger(),
                           'Gold(5)',
-                          'Crusty Bread']
-
+                          items.CrustyBread()]
         self.x = 1
         self.y = 2
+        self.hp = 100
 
     def print_inventory(self):
         print("Inventory:")
         for item in self.inventory:
             print('* ' + str(item))
 
+    def heal(self):
+        consumables = [item for item in self.inventory
+                       if isinstance(item, items.Consumable)]
+        if not consumables:
+            print("You don't have any items to heal you!")
+            return
+
+        for i, item in enumerate(consumables, 1):
+            print("Choose an item to use to heal: ")
+            print("{}. {}".format(i, item))
+
+        valid = False
+        while not valid:
+            choice = input("")
+            try:
+                to_eat = consumables[int(choice) - 1]
+                self.hp = min(100, self.hp + to_eat.healing_value)
+                self.inventory.remove(to_eat)
+                print("Current HP: {}".format(self.hp))
+                valid = True
+            except (ValueError, IndexError):
+                print("Invalid choice, try again.")
+
     def most_powerful_weapon(self):
         max_damage = 0
         best_weapon = None
-        best_weapon = self.most_powerful_weapon()
-        print("Your best weapon is your {}".format(best_weapon))
         for item in self.inventory:
             try:
                 if item.damage > max_damage:
@@ -27,6 +49,7 @@ class Player:
                     max_damage = item.damage
             except AttributeError:
                 pass
+
         return best_weapon
 
     def move(self, dx, dy):
@@ -44,3 +67,14 @@ class Player:
 
     def move_west(self):
         self.move(dx=-1, dy=0)
+
+    def attack(self):
+        best_weapon = self.most_powerful_weapon()
+        room = world.tile_at(self.x, self.y)
+        enemy = room.enemy
+        print("You use {} against {}!".format(best_weapon.name, enemy.name))
+        enemy.hp -= best_weapon.damage
+        if not enemy.is_alive():
+            print("You killed {}!".format(enemy.name))
+        else:
+            print("{} HP is {}.".format(enemy.name, enemy.hp))
