627353c3e45ff272c94fc602a281850304bbbc15
Jim Wilbur <45021912+JimboWW@users.noreply.github.com>
2021-07-31 16:18:03 -0700

    Update enemies.py

--- enemies.py
+++ enemies.py
@@ -1,39 +1,37 @@
 class Enemy:
-	def __init__( self ):
-		raise NotImplementedError("Do not create raw Enemy objects.")
-				
-	def __str__(self):
-		return self.name
-		
-	def is_alive(self):
-		return self.hp > 0
+    def __init__(self):
+        raise NotImplementedError("Do not create raw Enemy objects.")
 
-def intro_text( self ):
-	if self.enemy.is_alive():
-		return "A {} awaits!".format( self.enemy.name )
-	else:
-		return "You've defeated the {}.".format( self.enemy.name )				
-												
+    def __str__(self):
+        return self.name
+
+    def is_alive(self):
+        return self.hp > 0
+
+
 class GiantSpider(Enemy):
-	def __init__(self):
-		self.name = "Giant Spider"
-		self.hp = 10
-		self.damage = 2
-				
+    def __init__(self):
+        self.name = "Giant Spider"
+        self.hp = 10
+        self.damage = 2
+
+
 class Ogre(Enemy):
-	def __init__(self):
-		self.name = "Ogre"
-		self.hp = 30
-		self.damage = 10
-						
+    def __init__(self):
+        self.name = "Ogre"
+        self.hp = 30
+        self.damage = 10
+
+
 class BatColony(Enemy):
-	def __init__(self):
-		self.name = "Colony of bats"
-		self.hp = 100
-		self.damage = 4
-						
+    def __init__(self):
+        self.name = "Colony of bats"
+        self.hp = 100
+        self.damage = 4
+
+
 class RockMonster(Enemy):
-	def __init__(self):
-		self.name = "Rock Monster"
-		self.hp = 80
-		self.damage = 15
+    def __init__(self):
+        self.name = "Rock Monster"
+        self.hp = 80
+        self.damage = 15
