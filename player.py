60e254b95506ab67e92949de97b9959ca171ffe3
Jim Wilbur <45021912+JimboWW@users.noreply.github.com>
2021-07-30 17:59:06 -0700

    Update player.py

--- player.py
+++ player.py
@@ -1,1 +1,46 @@
+import items
 
+class Player:
+    def __init__(self):
+        self.inventory = [items.Rock(),
+                          items.Dagger(),
+                          'Gold(5)',
+                          'Crusty Bread']
+
+        self.x = 1
+        self.y = 2
+
+    def print_inventory(self):
+        print("Inventory:")
+        for item in self.inventory:
+            print('* ' + str(item))
+
+    def most_powerful_weapon(self):
+        max_damage = 0
+        best_weapon = None
+        best_weapon = self.most_powerful_weapon()
+        print("Your best weapon is your {}".format(best_weapon))
+        for item in self.inventory:
+            try:
+                if item.damage > max_damage:
+                    best_weapon = item
+                    max_damage = item.damage
+            except AttributeError:
+                pass
+        return best_weapon
+
+    def move(self, dx, dy):
+        self.x += dx
+        self.y += dy
+
+    def move_north(self):
+        self.move(dx=0, dy=-1)
+
+    def move_south(self):
+        self.move(dx=0, dy=1)
+
+    def move_east(self):
+        self.move(dx=1, dy=0)
+
+    def move_west(self):
+        self.move(dx=-1, dy=0)
