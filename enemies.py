d487d7d41bdb4c4a2322b883781f194f4071222d
Jim Wilbur <45021912+JimboWW@users.noreply.github.com>
2021-10-03 21:54:56 -0700

    Update enemies.py

--- enemies.py
+++ enemies.py
@@ -1,35 +1,27 @@
+#page( text_display )    Version 1.10    10-3-21
+#name, hp, damage
 class Enemy:
+    def __init__(self): raise NotImplementedError("Don't create Enemy objects.")
+    def __str__(self): return self.name
+    def is_alive(self): return self.hp > 0
+
+class HorseFly(Enemy):
     def __init__(self):
-        raise NotImplementedError("Do not create raw Enemy objects.")
-
-    def __str__(self):
-        return self.name
-
-    def is_alive(self):
-        return self.hp > 0
-
-
-class GiantSpider(Enemy):
-    def __init__(self):
-        self.name = "Giant Spider"
-        self.hp = 10
+        self.name = "Horse fly"
+        self.hp = 20
         self.damage = 2
-
-
-class Ogre(Enemy):
+class Rooster(Enemy):
     def __init__(self):
-        self.name = "Ogre"
+        self.name = "Rooster"
         self.hp = 30
         self.damage = 10
-
-
-class BatColony(Enemy):
+class BeeSwarm(Enemy):
     def __init__(self):
-        self.name = "Colony of bats"
+        self.name = "Swarm of Bees"
         self.hp = 100
         self.damage = 4
-
-
-class RockMonster(Enemy):
+class MeanDog(Enemy):
     def __init__(self):
-        
+        self.name = "MeanDog"
+        self.hp = 80
+        self.damage = 15     
